e831a684a4bae4e9437c1c7e3b6697cbd191e9c8
Patricio Bonetto <patricio.bonetto@outlook.com>
2020-03-29 18:44:10 -0300

    Added front-end

---
+++ phonebook/build/precache-manifest.aeaf456b8ce9e3491d12ff3ae37d709d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0eee245edd01a496b9b711d602fdb81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61e76e61acd85afa769b",
+    "url": "/static/css/main.134e9fdd.chunk.css"
+  },
+  {
+    "revision": "fdc00f3d2f4001d77518",
+    "url": "/static/js/2.9d147208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d147208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e76e61acd85afa769b",
+    "url": "/static/js/main.e241049e.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
