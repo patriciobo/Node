cef6227e3689bc6bd3a7d8bb91605908746c71fd
Patricio Bonetto <patricio.bonetto@outlook.com>
2020-03-29 12:06:43 -0300

    Initial commit

---
+++ phonebook/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+  ...
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
